4a34107a82d9b12a34c5103eb65e15d9997c08c4
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-09 13:45:05 -0400

    Bonus #17 - Car Inventory (First Commit)

---
+++ CarApp/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Car
+    {
+        //Serena Tindle--- Bonus #17 (Car Details)
+
+        //variables(class memebers)
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        //Method
+        public void CarDetails()
+        {
+            Console.WriteLine("Please enter the Make of the car:");
+            Console.ReadLine();
+            Console.WriteLine("Please enter the Model of the car:");
+            Console.ReadLine();
+            Console.WriteLine("Please enter the Year of the car:");
+            int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the Price of the car:");
+            double.Parse(Console.ReadLine());
+        }
+        
+       //Properties
+       public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+
+       public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+        
+       public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+
+       public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+
+       //Constructors
+       public Car() //default constructor
+        {
+            make = "";
+            model = "";
+            year = 0;
+            price = 0;
+        }
+        public Car(string CarMake, string CarModel, int CarYear, double CarPrice) //constructor w/parameters
+        {
+            make = CarMake; // class member and parameters 
+
+            model = CarModel;
+            year = CarYear;
+            price = CarPrice;
+        }
+
+
+
+
+
+    }
+}
---
+++ CarApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool ContinueProgram = true;
+            while (ContinueProgram == true) 
+            {
+                int NumOfCars;
+                Car CarInfo = new Car();
+                Console.WriteLine("Welcome to the Grand Circus Motors Admin Console");
+                Console.WriteLine("How many cars are you entering?");
+                NumOfCars=int.Parse(Console.ReadLine());
+                Car[] GCMotors = new Car[NumOfCars];
+
+                for (int i = 0; i < GCMotors.Length; i++)
+                {
+                    GCMotors[i] = new Car(); //creates a new object 
+                }
+                for (int i = 0; i < GCMotors.Length; i++)
+                {
+
+                    Console.WriteLine("Please enter the Make of the car:");
+                    GCMotors[i].Make =Console.ReadLine();
+
+                    Console.WriteLine("Please enter the Model of the car:");
+                    GCMotors[i].Model = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the Year of the car:");
+                    GCMotors[i].Year = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the Price of the car:");
+                    GCMotors[i].Price = double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Current Inventory:");
+                Console.WriteLine("Make \tModel \tYear \tPrice");
+                foreach (Car CarEntry in GCMotors)
+                {
+                    Console.WriteLine($"{CarEntry.Make} \t{CarEntry.Model} \t{CarEntry.Year} \t{CarEntry.Price}");
+                }
+
+                string UserOption; //Continue program or exit?
+                Console.WriteLine("Continue? (y/n)");
+                UserOption = Console.ReadLine();
+
+                if (UserOption == "N" || UserOption == "No" || UserOption == "no" || UserOption == "n"&& UserOption!="yes" || UserOption!= "y")
+                {
+                    ContinueProgram = false;
+                    Console.WriteLine("Thank you, Goodbye.");
+                }
+                Console.ReadKey();
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
